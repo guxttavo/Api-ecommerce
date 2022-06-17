4cc7b325b13bf5660d391f05105e7feeed03dcec
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-06-17 20:26:51 -0300

    Initial commit

---
+++ src/test/java/br/com/projetos/Apiecommerce/ApiEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetos.Apiecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
